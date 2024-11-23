a61cd047d45c26094b8d752ecd95a814878d6629
Du Than <du.than@relexsolutions.com>
2024-11-23 15:39:02 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,29 @@
+-- Use packer to manage plugins
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim' -- Packer itself
+  use 'nvim-lua/plenary.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate' -- Automatically update Treesitter parsers
+  }
+end)
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" for all parsers
+  ensure_installed = { "c", "javascript", "html", "css", "tsx", "typescript" },
+  -- Enable highlighting
+  highlight = {
+    enable = true,
+  },
+
+  -- Enable indentation based on Treesitter
+  indent = {
+    enable = true
+  }
+}
+
